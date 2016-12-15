430f34941c907fc37ce9fa3781e2e60812090037
Ainor Syahrizal <macintorch@gmail.com>
2016-12-15 22:17:03 +0800

    add play and pause button action

--- app/src/main/java/ainor/com/my/controlaudio/MainActivity.java
+++ app/src/main/java/ainor/com/my/controlaudio/MainActivity.java
@@ -3,12 +3,37 @@ package ainor.com.my.controlaudio;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    public void play(View view) {
+        mediaPlayer = MediaPlayer.create(this, R.raw.thunderstrike);
+
+        mediaPlayer.start();
+
+    }
+
+    public void pause(View view) {
+
+
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+        } else {
+            mediaPlayer.start();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
