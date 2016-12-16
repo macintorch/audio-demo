59c2f57a131548f1a0eee642cb8f296e639d64a6
Ainor Syahrizal <macintorch@gmail.com>
2016-12-16 21:29:38 +0800

    add progress bar

--- app/src/main/java/ainor/com/my/controlaudio/MainActivity.java
+++ app/src/main/java/ainor/com/my/controlaudio/MainActivity.java
@@ -8,12 +8,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.SeekBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
     AudioManager audioManager;
+    ProgressBar mProgressBar;
 
     public void play(View view) {
 
@@ -38,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mediaPlayer = MediaPlayer.create(this, R.raw.thunderstrike);
 
+        //Volume Control
+
         audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
         int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
@@ -54,6 +61,37 @@ public class MainActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 Log.i("Seekbar value", String.valueOf(i));
                 audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,i,0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        //Progress Bar of the music
+
+        final SeekBar progressBar = (SeekBar) findViewById(R.id.progressBar);
+
+        progressBar.setMax(mediaPlayer.getDuration());
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                progressBar.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        }, 0,100);
+
+        progressBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.i("Progress Bar value ", String.valueOf(i));
+                mediaPlayer.seekTo(i);
+
             }
 
             @Override
