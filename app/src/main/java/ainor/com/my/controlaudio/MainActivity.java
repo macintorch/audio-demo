15e47f365cddbeb2410c5e511c850254018386c0
Ainor Syahrizal <macintorch@gmail.com>
2016-12-16 15:28:31 +0800

    seekbar volume control

--- app/src/main/java/ainor/com/my/controlaudio/MainActivity.java
+++ app/src/main/java/ainor/com/my/controlaudio/MainActivity.java
@@ -1,5 +1,7 @@
 package ainor.com.my.controlaudio;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,9 +13,10 @@ import android.widget.SeekBar;
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
+    AudioManager audioManager;
 
     public void play(View view) {
-        mediaPlayer = MediaPlayer.create(this, R.raw.thunderstrike);
+
 
         mediaPlayer.start();
 
@@ -33,13 +36,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.thunderstrike);
+
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
 
         SeekBar volumeControl = (SeekBar) findViewById(R.id.seekBar);
+
+        volumeControl.setMax(maxVolume);
+        volumeControl.setProgress(currentVolume);
 
         volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 Log.i("Seekbar value", String.valueOf(i));
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,i,0);
             }
 
             @Override
